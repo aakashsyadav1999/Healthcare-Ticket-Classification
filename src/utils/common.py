f67e6409617f3dd8c8016ab521cab6bd790bad80
Aakash Yadav <Aakashsyadav1999@gmail.com>
2024-11-11 23:07:25 +0530

    Added MongoDB Client

---
+++ src/utils/common.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import json
+from pymongo import MongoClient
+
+
+# CommonUtils class to handle MongoDB connection
+class CommonUtils:
+    def __init__(self):
+        # Read MongoDB connection details from environment variables
+        mongo_host = os.getenv("MONGODB_URI")
+        mongo_port = int(os.getenv("MONGODB_PORT"))
+        mongo_db = os.getenv("MONGODB")
+        
+        # Initialize the MongoDB client and database connection
+        self.client = MongoClient(mongo_host, mongo_port)
+        self.db = self.client[mongo_db]
+
+    def insert_main_table(self):
+        # Use the initialized database connection
+        mongo_collection = os.getenv("MONGO_COLLECTION")
+        collection = self.db[mongo_collection]
+        return collection
+
+    def insert_into_urgency_table(self):
+        # Use the initialized database connection
+        mongo_collection = os.getenv("URGENCY_TABLE")
+        collection = self.db[mongo_collection]
+        return collection
+        
+    def close_connection(self):
+        if self.client:
+            self.client.close()
+            
+            
